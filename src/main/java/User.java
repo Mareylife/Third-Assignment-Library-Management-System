7db848935a3b4b689712f07230c6241b62f7a325
Mareylife <marey0204@gmail.com>
2023-03-02 22:03:13 +0330

    functions "rent book" & "return book" for User

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,12 +1,22 @@
+import java.util.ArrayList;
+
 public class User {
     //User should have a list of books
     //User should have a username and a password
+    private String username;
+    private String password;
 
-    public void rentBook(){
-        //TODO
+    public ArrayList<String> books;
+
+    public User(String username, String password)
+    {
+        this.username = username;
+        this.password = password;
     }
-
-    public void returnBook(){
-        //TODO
+    public void rentBook(String book){
+        books.add(book);
+    }
+    public void returnBook(String book){
+        books.remove(book);
     }
 }
