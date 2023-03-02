d0a2919aac9f5f57343e2ffcc924c913592fca11
Mareylife <marey0204@gmail.com>
2023-03-02 21:16:55 +0330

    setter and getter for Books

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,3 +1,42 @@
 public class Book {
     //Book should contain name,author,year of publish and ISBN
+    private String name;
+    private String author;
+    private int year;
+    private int isbn;
+
+    private boolean borrowed = false;
+
+    public Book (String name, String author, int year, int isbn)
+    {
+        this.name = name;
+        this.author = author;
+        this.year = year;
+        this.isbn = isbn;
+        borrowed = true;
+    }
+
+    public boolean isBorrowed() {
+        return borrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        this.borrowed = borrowed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
 }
